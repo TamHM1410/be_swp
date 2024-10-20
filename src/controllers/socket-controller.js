ed1e2bcde5f6922443f48647126d1ceb72c47fc2
TamHM1410 <hunhminhtam@gmail.com>
2024-10-20 23:07:06 +0700

    cc

--- src/controllers/socket-controller.js
+++ src/controllers/socket-controller.js
@@ -1,22 +1,36 @@
 const userSocketMap = {};
 const message_service=require('../services/messages-service')
-const socketController = (socket) => {
+const socketController = (socket,io) => {
+//   console.log('connected',(msg    )=>{
+//     io.emit('chat message', msg);
+//   })
+
   socket.on("user_connected", (userId) => {
-    userSocketMap[userId] = socket.id; // Lưu userId và socket.id vào đối tượng
+    userSocketMap[userId] = socket.id; 
     console.log(`User with ID: ${userId} is mapped to Socket ID: ${socket.id}`);
+
+  });
+  socket.on("user_typing", (msg) => {
+    console.log(msg)
+    socket.emit('liten',msg)
 
   });
   socket.on("join_conservation", (data) => {
     socket.join(data.conversationId);
     console.log(`User joined conversation: ${conversationId}`);
   });
+  
   socket.on("send_message",async (data)=>{
 
     let {message,sender,conversationId}=data
 
     let newMess=await message_service.createdNewMessage({message,sender,conversationId})
-    console.log(newMess,'new mess')
-  })
+    io.to(conversationId).emit('new_message', newMess);  
+})
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+});
 };
 
 module.exports = socketController;
